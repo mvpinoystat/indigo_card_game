587ab28aaab26d14b35c357c9b1eb66724cf4970
pinoystat <47047354+mvpinoystat@users.noreply.github.com>
2023-01-20 14:21:26 +0800

    - created separate player object of human and computer.

--- src/main/kotlin/cardindigo/Menu.kt
+++ src/main/kotlin/cardindigo/Menu.kt
@@ -4,24 +4,35 @@ enum class Players{
     HUMAN, COMPUTER
 }
 
+//a class of player:
+class Player(val kind: Players){
+    val cardsOnHand = mutableListOf<String>()
+    var score = 0
+
+}
+
 class Menu(private val cards: Cards){
+    //two types of players initialize
+    private val human = Player(Players.HUMAN)
+    private val computer = Player(Players.COMPUTER)
+
     fun execute(){
         println("Indigo Card Game")
         var hasSelected= false
-        var selection: String =""
+        var selection: String
         /*
          If selection == yes, then the player plays first (Example 1), and if no, then the computer plays
           first (Example 2).
          */
-        val selectedList = mapOf("YES" to Players.HUMAN, "NO" to Players.COMPUTER)
-        var currentPlayer: Players = Players.COMPUTER
+        val selectedList = listOf("YES" , "NO" )
+        var currentPlayer: Player = computer
         var terminate = false
         while(!hasSelected){
             println("Play First?")
             selection = readln()
-            if(selection.uppercase() in selectedList.keys){
+            if(selection.uppercase() in selectedList){
                 hasSelected = true
-                currentPlayer = selectedList[selection.uppercase()]!!
+                currentPlayer = if(selection.uppercase() == "YES") human else computer
             }else{
                 if(selection.uppercase() == "EXIT"){
                     hasSelected = true
@@ -35,24 +46,19 @@ class Menu(private val cards: Cards){
             print("Initial cards on the table: ")
             cards.get(4)
             //put in unique 6 cards on players and computer's hand:
-            cards.replenishHand(Players.HUMAN)
-            cards.replenishHand(Players.COMPUTER)
-            /*
-        println("The number of cards on card deck = ${cards.cardDeckBeingUsed.size}")
-        println("The number of cards on player hand = ${cards.cardsInPlayerHand.size}")
-        println("The number of cards on computer hand= ${cards.cardsInComputerHand.size}")
-        */
+            cards.replenishHand(human)
+            cards.replenishHand(computer)
 
             //Here, we start the game
             var isPlaying = true
             while (isPlaying) {
-                if (currentPlayer == Players.HUMAN) {
+                if (currentPlayer.kind == Players.HUMAN) {
                     cards.showCardTableStatus()
                     var looper = -2 //feedback regarding status of loop
                     if (cards.cardDeckBeingUsed.isNotEmpty() || cards.cardsInPlayerHand.isNotEmpty()) {
                         while (looper != 0) {//A 0 means acceptable input from player.
                             //the looper provides a feedback mechanism
-                            looper = cards.chooseCardToPlay(Players.HUMAN, looper)
+                            looper = cards.chooseCardToPlay(human, looper)
                             if(looper == 1){
                                 //this means that exit has been invoked:
                                 isPlaying = false
@@ -60,7 +66,7 @@ class Menu(private val cards: Cards){
                             }
 
                         }
-                        currentPlayer = Players.COMPUTER
+                        currentPlayer = computer
                     } else {
                         isPlaying = false
                     }
@@ -68,8 +74,8 @@ class Menu(private val cards: Cards){
                 } else {
                     cards.showCardTableStatus()
                     if (cards.cardDeckBeingUsed.isNotEmpty() || cards.cardsInComputerHand.isNotEmpty()) {
-                        cards.chooseCardToPlay(Players.COMPUTER, 0)
-                        currentPlayer = Players.HUMAN
+                        cards.chooseCardToPlay(computer, 0)
+                        currentPlayer = human
                     } else {
                         isPlaying = false
                     }
@@ -88,44 +94,44 @@ fun Cards.showCardTableStatus(){
     println(message)
 }
 
-fun Cards.printCardsInHand(hand:Players){
-    if(hand == Players.HUMAN){
+fun Cards.printCardsInHand(hand:Player){
+    if(hand.kind == Players.HUMAN){
         print("Cards in hand:")
-        for(i in this.cardsInPlayerHand.indices){
-            print(" ${i+1})${this.cardsInPlayerHand[i]}")
+        for(i in hand.cardsOnHand.indices){
+            print(" ${i+1})${hand.cardsOnHand[i]}")
         }
         println()
     }
-    if(hand == Players.COMPUTER){
+    if(hand.kind == Players.COMPUTER){
         print("Cards in hand:")
-        for(i in this.cardsInComputerHand.indices){
-            print(" ${i+1})${this.cardsInComputerHand[i]}")
+        for(i in hand.cardsOnHand.indices){
+            print(" ${i+1})${hand.cardsOnHand[i]}")
         }
         println()
     }
 
 }
 /** the player may choose a card or input exit */
-fun Cards.chooseCardToPlay(hand:Players, loopStatus:Int): Int{
-    if(hand == Players.HUMAN){
-        if(cardsInPlayerHand.isEmpty()){
+fun Cards.chooseCardToPlay(hand:Player, loopStatus:Int): Int{
+    if(hand.kind == Players.HUMAN){
+        if(hand.cardsOnHand.isEmpty()){
             this.replenishHand(hand)
         }
         if(loopStatus != -1) {//Do not print below if card selection is wrong
             this.printCardsInHand(hand)
         }
-        val selections = 1..this.cardsInPlayerHand.size
-        println("Choose a card to play (1-${this.cardsInPlayerHand.size}): ")
+        val selections = 1..hand.cardsOnHand.size
+        println("Choose a card to play (1-${hand.cardsOnHand.size}): ")
         val humanInput = readln()
-        //below, the player will choose an exit and it will return with no error with code 1
+        //below, the player will choose an exit, and it will return with no error with code 1
         if(humanInput.uppercase() == "EXIT") return 1
         else {
             return try {
                 val selected = humanInput.toInt()
                 if(selected in selections){
-                    val selectedCard = cardsInPlayerHand[selected-1]
+                    val selectedCard = hand.cardsOnHand[selected-1]
                     this.cardsShownOnTheTable.add(selectedCard)
-                    cardsInPlayerHand.remove(selectedCard)
+                    hand.cardsOnHand.remove(selectedCard)
                     0
                 } else{
                     -1 //return with error and ask player again
@@ -139,34 +145,34 @@ fun Cards.chooseCardToPlay(hand:Players, loopStatus:Int): Int{
     }
     else{
         //computer selects a random card from its hand:
-        if(cardsInComputerHand.isEmpty()){
+        //below hand is the computer hand
+        if(hand.cardsOnHand.isEmpty()){
             this.replenishHand(hand)
         }
-        val selectedCard = cardsInComputerHand.shuffled()[0]
+        val selectedCard = hand.cardsOnHand.shuffled()[0]
         println("Computer plays $selectedCard")
         cardsShownOnTheTable.add(selectedCard)
-        cardsInComputerHand.remove(selectedCard)
+        hand.cardsOnHand.remove(selectedCard)
 
         return 0
     }
 }
-
-fun Cards.replenishHand(hand: Players){
+fun Cards.replenishHand(hand: Player){
     if(this.cardDeckBeingUsed.isNotEmpty()) {
 
-        if (hand == Players.HUMAN) {
+        if (hand.kind == Players.HUMAN) {
             repeat(6) {
                 this.reshuffle()
                 val card = this.cardDeckBeingUsed[0]
-                this.cardsInPlayerHand.add(card)
+                hand.cardsOnHand.add(card)
                 this.cardDeckBeingUsed.remove(card)
             }
         }
-        if (hand == Players.COMPUTER) {
+        if (hand.kind == Players.COMPUTER) {
             repeat(6) {
                 this.reshuffle()
                 val card = this.cardDeckBeingUsed[0]
-                this.cardsInComputerHand.add(card)
+                hand.cardsOnHand.add(card)
                 this.cardDeckBeingUsed.remove(card)
             }
         }
